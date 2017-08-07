020fccfea17a1d5d87025428202e73853fd99992
Vinay Joshi <vinay01joshi@gmail.com>
2017-08-07 10:56:11 +0530

    Removing all files that are problems to make ng build --prod

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'http://cognitivelabapi20170805084655.azurewebsites.net/api',
-  baseUrl : 'http://cognitivelabapi20170805084655.azurewebsites.net'
-};
