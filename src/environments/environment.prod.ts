363c320b6d94a0c8e7d91762614b88fb1a96613b
Vinay Joshi <vinay01joshi@gmail.com>
2017-08-07 15:44:30 +0530

    Some minor changes in environment and package.json file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://cognitivelabapi20170805084655.azurewebsites.net/api',
-  baseUrl : 'http://cognitivelabapi20170805084655.azurewebsites.net'
+  apiBaseUrl: 'https://cognitivelabapi20170805084655.azurewebsites.net/api',
+  baseUrl : 'https://cognitivelabapi20170805084655.azurewebsites.net'
 };
